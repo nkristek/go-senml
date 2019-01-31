29625d8c84c059e03597f0a9b18754aa7000d7b8
Niclas Kristek <nkristek@users.noreply.github.com>
2019-01-31 12:17:09 +0100

    Remove unnecessary unit tests

--- senml_test.go
+++ senml_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // https://tools.ietf.org/html/rfc8428#section-5.1.3
-const jsonDataUnresolved string = `[
+const jsonData string = `[
 	{"bn":"urn:dev:ow:10e2073a01080063","bt":1.320067464e+09,
 	 "bu":"%RH","v":20},
 	{"u":"lon","v":24.30621},
@@ -24,38 +24,8 @@ const jsonDataUnresolved string = `[
 	{"u":"lat","t":180,"v":60.07967}
   ]`
 
-// https://tools.ietf.org/html/rfc8428#section-5.1.4
-const jsonDataResolved string = `[
-	{"n":"urn:dev:ow:10e2073a01080063","u":"%RH","t":1.320067464e+09,
-	 "v":20},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lon","t":1.320067464e+09,
-	 "v":24.30621},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lat","t":1.320067464e+09,
-	 "v":60.07965},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"%RH","t":1.320067524e+09,
-	 "v":20.3},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lon","t":1.320067524e+09,
-	 "v":24.30622},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lat","t":1.320067524e+09,
-	 "v":60.07965},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"%RH","t":1.320067584e+09,
-	 "v":20.7},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lon","t":1.320067584e+09,
-	 "v":24.30623},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lat","t":1.320067584e+09,
-	 "v":60.07966},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"%EL","t":1.320067614e+09,
-	 "v":98},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"%RH","t":1.320067644e+09,
-	 "v":21.2},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lon","t":1.320067644e+09,
-	 "v":24.30628},
-	{"n":"urn:dev:ow:10e2073a01080063","u":"lat","t":1.320067644e+09,
-	 "v":60.07967}
-  ]`
-
 // https://tools.ietf.org/html/rfc8428#section-7
-const xmlDataUnresolved string = `<sensml xmlns="urn:ietf:params:xml:ns:senml">
+const xmlData string = `<sensml xmlns="urn:ietf:params:xml:ns:senml">
 	<senml bn="urn:dev:ow:10e2073a0108006:" bt="1.276020076001e+09"
 	bu="A" bver="5" n="voltage" u="V" v="120.1"></senml>
 	<senml n="current" t="-5" v="1.2"></senml>
@@ -67,7 +37,7 @@ const xmlDataUnresolved string = `<sensml xmlns="urn:ietf:params:xml:ns:senml">
   </sensml>`
 
 func TestDecodeJSON(t *testing.T) {
-	_, err := senml.Decode([]byte(jsonDataUnresolved), senml.JSON)
+	_, err := senml.Decode([]byte(jsonData), senml.JSON)
 	if err != nil {
 		t.Error("Decoding JSON failed: ", err)
 		return
@@ -75,7 +45,7 @@ func TestDecodeJSON(t *testing.T) {
 }
 
 func TestDecodeXML(t *testing.T) {
-	_, err := senml.Decode([]byte(xmlDataUnresolved), senml.XML)
+	_, err := senml.Decode([]byte(xmlData), senml.XML)
 	if err != nil {
 		t.Error("Decoding XML failed: ", err)
 		return
@@ -90,7 +60,7 @@ func TestDecodeInvalidFormat(t *testing.T) {
 }
 
 func TestEncodeJSON(t *testing.T) {
-	message, err := senml.Decode([]byte(jsonDataUnresolved), senml.JSON)
+	message, err := senml.Decode([]byte(jsonData), senml.JSON)
 	if err != nil {
 		t.Error("Decoding JSON failed: ", err)
 		return
@@ -107,7 +77,7 @@ func TestEncodeJSON(t *testing.T) {
 }
 
 func TestEncodeXML(t *testing.T) {
-	message, err := senml.Decode([]byte(xmlDataUnresolved), senml.XML)
+	message, err := senml.Decode([]byte(xmlData), senml.XML)
 	if err != nil {
 		t.Error("Decoding XML failed: ", err)
 		return
@@ -124,121 +94,11 @@ func TestEncodeXML(t *testing.T) {
 }
 
 func TestEncodeInvalidFormat(t *testing.T) {
-	message, err := senml.Decode([]byte(xmlDataUnresolved), senml.XML)
-	if err != nil {
-		t.Error("Decoding XML failed: ", err)
-		return
-	}
-
-	_, err = message.Encode(-1)
+	message := senml.Message{}
+	_, err := message.Encode(-1)
 	if err == nil {
 		t.Error("Encoding message to an invalid format should result in an error")
 		return
-	}
-}
-
-func TestResolveExampleData(t *testing.T) {
-	message, err := senml.Decode([]byte(jsonDataUnresolved), senml.JSON)
-	if err != nil {
-		t.Error("Decoding JSON failed: ", err)
-		return
-	}
-
-	resolvedDataMessage, err := senml.Decode([]byte(jsonDataResolved), senml.JSON)
-	if err != nil {
-		t.Error("Decoding JSON failed: ", err)
-		return
-	}
-
-	resolvedMessage, err := message.Resolve()
-	if err != nil {
-		t.Error("Resolving message failed: ", err)
-		return
-	}
-
-	compareMessages(t, resolvedMessage, resolvedDataMessage)
-}
-
-func compareMessages(t *testing.T, firstMessage senml.Message, secondMessage senml.Message) {
-	if len(firstMessage.Records) != len(secondMessage.Records) {
-		t.Error("Unequal amount of records")
-		return
-	}
-	for i := 0; i < len(firstMessage.Records); i++ {
-		firstMessageRecord := firstMessage.Records[i]
-		secondMessageRecord := secondMessage.Records[i]
-
-		if firstMessageRecord.Name == nil && secondMessageRecord.Name != nil || firstMessageRecord.Name != nil && secondMessageRecord.Name == nil {
-			t.Error("Name of resolved message is not set")
-			return
-		} else if firstMessageRecord.Name != nil && secondMessageRecord.Name != nil && *firstMessageRecord.Name != *secondMessageRecord.Name {
-			t.Error("Name of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.Unit == nil && secondMessageRecord.Unit != nil || firstMessageRecord.Unit != nil && secondMessageRecord.Unit == nil {
-			t.Error("Unit of resolved message is not set")
-			return
-		} else if firstMessageRecord.Unit != nil && secondMessageRecord.Unit != nil && *firstMessageRecord.Unit != *secondMessageRecord.Unit {
-			t.Error("Unit of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.Value == nil && secondMessageRecord.Value != nil || firstMessageRecord.Value != nil && secondMessageRecord.Value == nil {
-			t.Error("Value of resolved message is not set")
-			return
-		} else if firstMessageRecord.Value != nil && secondMessageRecord.Value != nil && *firstMessageRecord.Value != *secondMessageRecord.Value {
-			t.Error("Value of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.BoolValue == nil && secondMessageRecord.BoolValue != nil || firstMessageRecord.BoolValue != nil && secondMessageRecord.BoolValue == nil {
-			t.Error("BoolValue of resolved message is not set")
-			return
-		} else if firstMessageRecord.BoolValue != nil && secondMessageRecord.BoolValue != nil && *firstMessageRecord.BoolValue != *secondMessageRecord.BoolValue {
-			t.Error("BoolValue of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.StringValue == nil && secondMessageRecord.StringValue != nil || firstMessageRecord.StringValue != nil && secondMessageRecord.StringValue == nil {
-			t.Error("StringValue of resolved message is not set")
-			return
-		} else if firstMessageRecord.StringValue != nil && secondMessageRecord.StringValue != nil && *firstMessageRecord.StringValue != *secondMessageRecord.StringValue {
-			t.Error("StringValue of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.DataValue == nil && secondMessageRecord.DataValue != nil || firstMessageRecord.DataValue != nil && secondMessageRecord.DataValue == nil {
-			t.Error("DataValue of resolved message is not set")
-			return
-		} else if firstMessageRecord.DataValue != nil && secondMessageRecord.DataValue != nil && *firstMessageRecord.DataValue != *secondMessageRecord.DataValue {
-			t.Error("DataValue of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.Sum == nil && secondMessageRecord.Sum != nil || firstMessageRecord.Sum != nil && secondMessageRecord.Sum == nil {
-			t.Error("Sum of resolved message is not set, first: ", firstMessageRecord.Sum, ", second: ", secondMessageRecord.Sum)
-			return
-		} else if firstMessageRecord.Sum != nil && secondMessageRecord.Sum != nil && *firstMessageRecord.Sum != *secondMessageRecord.Sum {
-			t.Error("Sum of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.Time == nil && secondMessageRecord.Time != nil || firstMessageRecord.Time != nil && secondMessageRecord.Time == nil {
-			t.Error("Time of resolved message is not set")
-			return
-		} else if firstMessageRecord.Time != nil && secondMessageRecord.Time != nil && *firstMessageRecord.Time != *secondMessageRecord.Time {
-			t.Error("Time of resolved message doesnt match")
-			return
-		}
-
-		if firstMessageRecord.UpdateTime == nil && secondMessageRecord.UpdateTime != nil || firstMessageRecord.UpdateTime != nil && secondMessageRecord.UpdateTime == nil {
-			t.Error("UpdateTime of resolved message is not set")
-			return
-		} else if firstMessageRecord.UpdateTime != nil && secondMessageRecord.UpdateTime != nil && *firstMessageRecord.UpdateTime != *secondMessageRecord.UpdateTime {
-			t.Error("UpdateTime of resolved message doesnt match")
-			return
-		}
 	}
 }
 
